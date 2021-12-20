3edfe86baa239203217d8bbe5c60d5e1519cafbb
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-12-20 15:44:03 +0000

    Further clean up of naming and group class methods

--- lib/slack_wrapper/channel.rb
+++ lib/slack_wrapper/channel.rb
@@ -11,14 +11,14 @@ module SlackWrapper
     end
 
     def post_a_message!(text:)
-      slack_client.chat_postMessage(
+      client.chat_postMessage(
         channel: identifier,
         text: text
       )
     end
 
     def post_a_scheduled_message!(text:, time:)
-      slack_client.chat_scheduleMessage(
+      client.chat_scheduleMessage(
         channel: identifier,
         text: text,
         post_at: time
@@ -26,58 +26,60 @@ module SlackWrapper
     end
 
     def pin_a_message!(message_timestamp:)
-      slack_client.pins_add(
+      client.pins_add(
         channel: identifier,
         timestamp: message_timestamp
       )
     end
 
-    def current_topic
-      slack_client.conversations_info(
+    def topic
+      client.conversations_info(
         channel: identifier
       ).dig(:channel, :topic, :value)
     end
 
-    def current_members
-      slack_client.conversations_members(
+    def members
+      client.conversations_members(
         channel: identifier
       ).members
     end
 
-    def self.create(is_private:, channel_name:)
-      slack_client.conversations_create(
-        name: channel_name,
-        is_private: is_private
-      )
-    end
-
     def invite_users!(users:)
-      slack_client.conversations_invite(
+      client.conversations_invite(
         channel: identifier,
         users: users.join(',')
       )
     end
 
     def invite_user!(user:)
-      slack_client.conversations_invite(
+      client.conversations_invite(
         channel: identifier,
         users: user
       )
     end
 
     def create_topic!(summary:)
-      slack_client.conversations_setTopic(
+      client.conversations_setTopic(
         channel: identifier,
         topic: summary
       )
     end
 
-    def self.slack_client
-      Slack::Web::Client.new(token: @token || ENV['SLACK_TOKEN'])
+    def client
+      @client ||= self.class.client
     end
 
-    def slack_client
-      @slack_client ||= self.class.slack_client
+    class << self
+      def client
+        Slack::Web::Client.new(token: @token || ENV['SLACK_TOKEN'])
+      end
+
+      def create!(name:, is_private: false)
+        client.conversations_create(
+          name: name,
+          is_private: is_private
+        )
+      end
     end
   end
 end
